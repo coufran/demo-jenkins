f99c3a07d420b582627a43d2264d57ce155c7579
coufran <coufran@qq.com>
2021-02-22 17:56:51 +0800

    add pid file

--- src/main/java/cn/coufran/demo/jenkins/DemoApplication.java
+++ src/main/java/cn/coufran/demo/jenkins/DemoApplication.java
@@ -2,10 +2,13 @@ package cn.coufran.demo.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication application = new SpringApplication(DemoApplication.class);
+        application.addListeners(new ApplicationPidFileWriter("pid"));
+        application.run(args);
     }
 }
